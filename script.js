ec030da4a0b73a9e639d46311221d42d4962914d
willl85 <ngowilfried@gmail.com>
2023-08-28 23:22:12 +0200

    Adding Movies to the DOM

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 
 const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -16,6 +17,44 @@ async function getMovies(url) {
   const data = await res.json();
 
   console.log(data.results);
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+
+    movieEl.innerHTML = `
+        <img
+          src=${IMG_PATH + poster_path}
+          alt=${title}
+        />
+        <div class="movie-info">
+          <h3>${title}</h3>
+          <span class=${getClassByRate(vote_average)}>${vote_average.toFixed(1)}</span>
+        </div>
+        <div class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </div>
+    `;
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
 }
 
 form.addEventListener('submit', (e) => {
